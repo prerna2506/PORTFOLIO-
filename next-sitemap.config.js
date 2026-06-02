fcf3105dd54cff594306c079e612bed291b10df7
Prerna Singh <158843520+prerna2506@users.noreply.github.com>
2026-06-02 18:11:08 +0530

    Fixed empty sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   siteUrl: 'https://portfolio-phi-seven-ba7rxhbca6.vercel.app',
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
+
+  additionalPaths: async (config) => [
+    await config.transform(config, '/'),
+  ],
 }
